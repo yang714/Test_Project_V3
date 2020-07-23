aba44e33e3d4052e67b83f7529bec754bf18e86d
yang714 <ceerr714@gmail.com>
2020-07-24 03:52:29 +0800

    Test_orderFID

--- src/Order_Table_Model/Order_Table_Action.java
+++ src/Order_Table_Model/Order_Table_Action.java
@@ -4,17 +4,21 @@ import HibernateUtilpack.HibernateUtil;
 import Memu_model.Memu_M;
 import Order_Table.Order_Table_Interface;
 import Test_HIB.MemuEntity;
+import Test_HIB.TableKindEntity;
+import org.hibernate.Query;
 import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 
 import javax.annotation.Resource;
-import javax.management.Query;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 
 public class Order_Table_Action implements Order_Table_Interface {
     @Resource(name = "DataS")
@@ -42,20 +46,40 @@ public class Order_Table_Action implements Order_Table_Interface {
 
     @Override
     public int FindTable_ID(String tablename, String tablenumber) throws SQLException {
-        Connection cnn=ds.getConnection();
-        PreparedStatement ps=cnn.prepareStatement(
-                "SELECT  Table_ID\n" +
-                        " \n" +
-                        "  FROM Table_Kind\n" +
-                        "  where Table_Name=? and Table_number=?");
-        ps.setString(1, tablename);
-        ps.setString(2,tablenumber);
-        ResultSet rs=ps.executeQuery();
+        /*************************************/
+        Session session=HibernateUtil.getSessionFactory().openSession();
+        Query query= session.createQuery("select TKE from TableKindEntity AS TKE where TKE.tableName=?1 and TKE.tableNumber=?2");
+       query.setParameter(1,tablename);
+       query.setParameter(2,Integer.valueOf(tablenumber));
+//        Query query2= session.createQuery("select TKE from TableKindEntity as TKE" );
         int t_id=0;
-        while(rs.next()){
-            t_id=rs.getInt(1);
+        Iterator test=query.list().iterator();
+        while(test.hasNext()){
+            TableKindEntity jojo=  (TableKindEntity)test.next();
+//            System.out.println(jojo.getTableId());
+            t_id=jojo.getTableId();
         }
+
+
+//        Iterator us=query.list().iterator();
+
+//        t_id=((TableKindEntity)query).getTableId();
         return t_id;
+        /*************************************/
+//        Connection cnn=ds.getConnection();
+//        PreparedStatement ps=cnn.prepareStatement(
+//                "SELECT  Table_ID\n" +
+//                        " \n" +
+//                        "  FROM Table_Kind\n" +
+//                        "  where Table_Name=? and Table_number=?");
+//        ps.setString(1, tablename);
+//        ps.setInt(2, Integer.parseInt(tablenumber));
+//        ResultSet rs=ps.executeQuery();
+//        int t_id=0;
+//        while(rs.next()){
+//            t_id=rs.getInt(1);
+//        }
+//        return t_id;
     }
 
     @Override
